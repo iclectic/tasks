9b6dbba8d0689d88fb93db5e5003c233c5eb589a
iclectic <tansonestost@yahoo.com>
2024-09-16 23:40:05 +0100

    Shopping list, Counter and Idea screen tab icons added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,40 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Feather from '@expo/vector-icons/Feather';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { theme } from "../theme";
 
 export default function Layout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-        </Stack>
+        <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean}}>
+            <Tabs.Screen 
+             name="index" 
+             options={{ 
+                title: "Shopping List",
+                tabBarIcon: ({ color, size }) => {
+                    return <FontAwesome5 name="list-ul" size={size} color={color} />; 
+                    },
+             }} 
+            />
+            <Tabs.Screen 
+                name="counter" 
+                options={{ 
+                    title: "Counter", 
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="clock" size={size} color={color} />
+                    ),
+                }} 
+                />
+            <Tabs.Screen 
+                name="idea" 
+                options=
+                {{ 
+                    title: "Idea",
+                    tabBarIcon: ({ color, size}) => (
+                        <MaterialCommunityIcons name="lightbulb-on-outline" size={size} color={color} />
+                    )
+                }} 
+                />
+        </Tabs>
     );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,12 @@ import ShoppingListItem from "../components/ShoppingListItem";
 import { StyleSheet, 
          View, 
         } from "react-native";
-import {Link} from "expo-router";
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link href="/counter" style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
-       Go to counter 
-      </Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Sugar" isCompleted />
