8ded17f775117ff047a8fcd99877422b7a522ee8
iclectic <tansonestost@yahoo.com>
2024-09-18 00:55:11 +0100

    Text Input addition

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,50 @@
 import ShoppingListItem from "../components/ShoppingListItem";
-import { StyleSheet, 
-         View, 
+import { StyleSheet, View, TextInput,
         } from "react-native";
+import { theme } from "../theme";
+import { useState } from "react";
 
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
 
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Sugar" },
+]
 
 export default function App() {
+  const [shoppingList, setShoppingList] = 
+    useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toTimeString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Sugar" isCompleted />
+      <TextInput 
+      placeholder="E.g. Coffee" 
+      style={styles.textInput} 
+      value={value}
+      onChangeText = {setValue}
+      returnKeyType="done"
+      onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem name={item.name} key={item.id} />
+      ))}
+     
     </View>
   );
 }
@@ -19,6 +53,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
