a84cf41779a793a320bc191eb0dccf4156ecb40d
iclectic <tansonestost@yahoo.com>
2024-09-23 20:08:53 +0100

    started creatng a timer feature

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet, TouchableOpacity, Alert} from "react-native";
+import {useState, useEffect} from "react";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Notifications from "expo-notifications";
@@ -7,6 +8,18 @@ import * as Device from "expo-device";
 
 
 export default function CounterScreen() {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsElapsed(val => val + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
     if (result === "granted") {
@@ -30,6 +43,7 @@ export default function CounterScreen() {
 
   return (
     <View style={styles.container}>
+      <Text>{secondsElapsed}</Text>
       <TouchableOpacity 
         style={styles.button} 
         activeOpacity={0.8} 
