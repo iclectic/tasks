f2a512b9c21a2f1f0a25b5f10a6cf6609fe9a032
iclectic <tansonestost@yahoo.com>
2024-09-28 15:42:49 +0100

    Storing Timer History

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,21 +1,63 @@
-import { Text, View, StyleSheet} from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import {useState, useEffect} from "react";
+import { getFromStorage } from "../../utils/storage";
+import  { countdownStorageKey, PersistedCountdownState } from ".";
+import { format } from "date-fns";
+import { theme } from "../../theme";
 
+const fullDateFormat = "LLL d yyyy, h:mm aaa";
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+
+    useEffect(() => {
+      const init = async () => {
+        const value = await getFromStorage(countdownStorageKey);
+        setCountdownState(value);
+      };
+      init();
+    }, []);
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countdownState?.completedAtTimestamps}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>No History</Text>
+        </View>
+      }
+       renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>{format(item, fullDateFormat)}</Text>
+          </View>
+        )}
+      />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
   },
-  text: {
-    fontSize: 24,
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    backgroundColor: theme.colorGrey,
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 8,
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+  listEmptyContainer: {
+   justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
   },
 });
